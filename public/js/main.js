13e8145cdb98c2a70bc431ca60c506a9a71faf40
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-04-24 17:17:02 -0600

    fixed express static structure

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 var app = angular.module('app', []);
 
-app.constroller('mainCntrl', ['$scope', function($scope) {
+app.controller('mainCntrl', ['$scope', function($scope) {
     $scope.greet = 'Hello Angular';
 }]);
