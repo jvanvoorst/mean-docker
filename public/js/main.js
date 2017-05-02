41de3dc515b0b06a53c98764b7dc608d43bef723
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-05-02 17:02:41 -0600

    initial

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,46 @@
 /* jshint esversion: 6 */
 
-const app = angular.module('app', []);
+const app = angular.module('app', ['ngRoute']);
 
-app.controller('mainCntrl', ['$scope', ($scope) => {
-    $scope.name = 'Hello Angular';
+app.config(function ($routeProvider) {
+
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/first.html',
+        controller: 'mainCntrl'
+    })
+    .when('/second', {
+        templateUrl: 'views/second.html',
+        controller: 'secondCntrl'
+    })
+    .when('/second/:num', {
+        templateUrl: 'views/second.html',
+        controller: 'secondCntrl'
+    });
+
+});
+
+app.service('nameService', function() {
+    // let self = this;
+    this.name = 'Justin V';
+    this.nameLength = () => this.name.length;
+});
+
+app.controller('mainCntrl', ['$scope', '$log', 'nameService', function($scope, $log, nameService) {
+    $scope.name = nameService.name;
+
+    $scope.$watch('name', () => nameService.name = $scope.name );
+
+    $log.log(nameService.name);
+    $log.log(nameService.nameLength());
+
 }]);
 
-app.controller('secondCntrl', ['$scope', ($scope) => {
-    $scope.name = 'Second';
-}]);
+app.controller('secondCntrl', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
+    $scope.name = nameService.name;
 
-//adding something
+    $scope.$watch('name', () => nameService.name = $scope.name );
+
+    $scope.num = $routeParams.num || 'default';
+
+}]);
